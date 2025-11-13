b34034e2f3c919de14b3856d3ea35446cc2b1e4f
Ksenia Pishikova <sreksrekov26@gmail.com>
2025-11-13 23:11:13 +1000

    fixed icons

--- main.cpp
+++ main.cpp
@@ -6,16 +6,8 @@
 int main(int argc, char *argv[]){
 
     QApplication app(argc, argv);
-    QString iconPath = ":icons/icon.png";
-    if (QFile::exists(iconPath)) {
-        app.setWindowIcon(QIcon(iconPath));
-    } else {
-        // Fallback - попробовать относительный путь
-        iconPath = "icons/icon.png";
-        if (QFile::exists(iconPath)) {
-            app.setWindowIcon(QIcon(iconPath));
-        }
-    }
+    QIcon icon(":/icons/app.png");
+    app.setWindowIcon(icon);
     MainWindow window;
     window.show();
     return app.exec();
